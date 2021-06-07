7971242e4f36cf54365175898714650171b80d61
Aaron <ponyminikoni@gmail.com>
2021-06-07 19:08:04 +0300

    Initial Commit

---
+++ Meteostatic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meteostatic
+//
+//  Created by Aaron Gulbudagyan on 07.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meteostatic/MeteostaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteostaticApp.swift
+//  Meteostatic
+//
+//  Created by Aaron Gulbudagyan on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeteostaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
