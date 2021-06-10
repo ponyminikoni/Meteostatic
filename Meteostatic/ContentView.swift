e42d7cd698ff6bd700c55e9038b8bb258c269064
Aaron <ponyminikoni@gmail.com>
2021-06-10 11:41:48 +0300

    initial commit

--- Meteostatic/ContentView.swift
+++ Meteostatic/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let width = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+                .ignoresSafeArea()
+            VStack(spacing: width / 3) {
+                Text("City")
+                    .font(.custom("Courier New", size: 30))
+                    .opacity(0.5)
+                Image("sun")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: width / 2, height: width / 2, alignment: .center)
+                HStack(spacing: width / 2.5) {
+                    Text("Temp")
+                    Text("Wind")
+                }
+                .font(.custom("Courier New", size: 20))
+            }
+        }
     }
 }
 
